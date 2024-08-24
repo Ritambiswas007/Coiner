9c34be716d98f37463f8e7f0fecadf1758766d74
Ritambiswas007 <114689483+Ritambiswas007@users.noreply.github.com>
2024-08-24 18:53:06 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import CoinFlip from './components/coinFlip';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coin Flip Game</h1>
+        <CoinFlip />
+      </header>
+    </div>
+  );
+}
+
+export default App;
